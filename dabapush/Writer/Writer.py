815099a83e8954fa6dd84f86f6312aa2681f2915
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-07-28 15:12:43 +0200

    [add] write loop/chunkSize

--- dabapush/Writer/Writer.py
+++ dabapush/Writer/Writer.py
@@ -5,9 +5,12 @@ from loguru import logger as log
 class Writer(object):
     
     buffer: pd.DataFrame
-    
+    chunkSize: int
+
+    # TODO: get chunksize from config
     def __init__(self):
         self.buffer = pd.DataFrame()
+        self.chunkSize = 100
 
     def write(self, df: pd.DataFrame):
         self.buffer = pd.concat([self.buffer, df], ignore_index=True, sort=False)
@@ -15,6 +18,9 @@ class Writer(object):
  
     def __writeHandler(self):
         log.info(f'Buffer now contains {len(self.buffer)} records.')
+        # Call write persistance loop to empty buffer
+        while (len(self.buffer) > self.chunkSize):
+            self.persist()
 
     @abc.abstractmethod
     def persist(self) -> None:
