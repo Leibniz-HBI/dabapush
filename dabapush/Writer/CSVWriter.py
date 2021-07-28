898044344e59d274ec665abbe70f60e9f27065c0
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-07-28 15:12:53 +0200

    [add] CSVWriter

---
+++ dabapush/Writer/CSVWriter.py
@@ -0,0 +1,16 @@
+import csv
+from loguru import logger as log
+from pathlib import Path
+from .Writer import Writer
+
+class CSVWriter(Writer):
+    
+    def __init__(self):
+        super().__init__()
+
+    def persist(self):
+        self.buffer,last_row=self.buffer.drop(self.buffer.tail(self.chunkSize).index),self.buffer.tail(self.chunkSize)
+        log.info(f'Persisted {len(last_row)} records')
+        with Path('res.csv').open('a') as file:
+            last_row.to_csv(file, index=False)
+        return
