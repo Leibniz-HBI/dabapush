84a546d0aa25578bd7b8a5164afd9b7466483e79
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-07-28 09:31:38 +0200

    [update] click options and args

--- dabapush/dabapush.py
+++ dabapush/dabapush.py
@@ -13,9 +13,29 @@ from .writer import writer
 
 @click.command()
 @click.argument('input', type=click.Path(exists=True))
+@click.option('--pattern', default='*.json', help='file extension to look for')
 @click.option('--host', default='localhost', help='Host address of the dabase too write to.')
 @click.option('--port', default='5432', help='Host port of the dabase-server.')
 @click.option('--dbname', default='dabapushed', help='Name of the dabase too write to.')
+@click.option('--n_workers', default=cpu_count, help="Number of worker threads to read/write data")
+@click.option('--reader', default='Twacapic', help='python class to read stuff (see docs). Possible values: Twacapic')
+@click.option('--writer', help='python class to write stuff (see docs)')
+@click.option('--recursive', '-r', help='should the reader recurse in subdirectories? Default: TRUE.', is_flag=True)
+@click.option('--logfile', help='file to logi in (optional)')
+@click.option('--loglevel', default='INFO', help='the level to log, yk')
+def run(
+    input: str,
+    pattern: str,
+    reader: str,
+    writer: str,
+    host: str,
+    port: str,
+    dbname: str,
+    n_workers: str,
+    recursive: bool,
+    logfile: str,
+    loglevel: str
+):
     # load config
     config = yaml.load(Path('config.yml').open(), Loader=yaml.FullLoader)
     
