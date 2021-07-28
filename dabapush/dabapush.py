3cfee222fc5a6f851bfab1c918eb0c16facb80fe
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-07-28 09:27:44 +0200

    [add] config file and reader

--- dabapush/dabapush.py
+++ dabapush/dabapush.py
@@ -6,9 +6,11 @@ import click
 @click.option('--host', default='localhost', help='Host address of the dabase too write to.')
 @click.option('--port', default='5432', help='Host port of the dabase-server.')
 @click.option('--dbname', default='dabapushed', help='Name of the dabase too write to.')
-@click.option('--n_workers', default='4', help="Number of worker threads to read/write data")
-def run(input, host, port, dbname, n_workers):
-    # List all of the files in dir
+    # load config
+    config = yaml.load(Path('config.yml').open(), Loader=yaml.FullLoader)
+    
+    log.debug('Using this configuration', config)
+    
     # start $n_workers workers to read the data
     # if JSON accecssor is given, apply it to each loaded file
     print(f'{input} will be written to {host}:{port}/{dbname} with {n_workers} parallel threads')
