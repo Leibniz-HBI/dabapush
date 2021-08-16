c278674349d2cce5ba455c291ba996f9641f4652
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-08-16 09:00:57 +0200

    [add] stubs for new CL interface

--- dabapush/Reader/TwacapicReader.py
+++ dabapush/Reader/TwacapicReader.py
@@ -1,16 +1,31 @@
 from pathlib import Path
 from json import load
+from typing import Dict
 import pandas as pd
 from loguru import logger as log
 from .Reader import Reader
 
 class TwacapicReader(Reader):
-    
+
+    @staticmethod
+    def setup(path: Path) -> None:
+        # what did I want do here?
+        return
+   
+    @staticmethod
+    def getSchema() -> list(str):
+        if (TwacapicReader.__setup == False):
+            raise 'Tryed to use TwacapicReader without setting it up first'
+        if (TwacapicReader.config is None):
+            raise 'Tryed to use TwacapicReader without proper configuration'
+        return [""]
+
     def __init__(self, path: Path, property: str = 'data'):
         super().__init__(path)
         self.property = property
     
     def read(self):
+        schema = TwacapicReader.getSchema()
         data = None
         with self.path.open('r') as file:
             try:
