1365ad41eeb8fa357951f1a954c59402e6e67640
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-08-27 17:21:53 +0200

    [add] fixture

--- tests/Configuration/test_Configuration.py
+++ tests/Configuration/test_Configuration.py
@@ -1,38 +1,40 @@
 from pytest import fixture, skip
 from dabapush.Configuration.Configuration import Configuration
 
-def test_Configuration():
-    skip()
+@fixture
+def conf():
+    return Configuration()
+
 # should serialize
-def test_serialize():
+def test_serialize(conf: Configuration):
     skip()
 # should deserialize
-def test_deserialize():
+def test_deserialize(conf: Configuration):
     skip()
 # should get a reader plugin and return it
-def test_get_reader():
+def test_get_reader(conf: Configuration):
     skip()
 # should register a reader plugin and assign it a name
-def test_register_reader():
+def test_register_reader(conf: Configuration):
     skip()
 # should remove reader plugin by name
-def test_remove_reader():
+def test_remove_reader(conf: Configuration):
     skip()
 # should list registered reader plugins
-def test_list_reader():
+def test_list_reader(conf: Configuration):
     skip()
 # should register writer plugins and assign a name
-def test_register_writer():
+def test_register_writer(conf: Configuration):
     skip()
 # should remove writer plugins by name
-def test_remove_writer():
+def test_remove_writer(conf: Configuration):
     skip()
 # should list registered writer plugins
-def test_list_writers():
+def test_list_writers(conf: Configuration):
     skip()
 # should get a writer plugin and return it
-def test_get_writer():
+def test_get_writer(conf: Configuration):
     skip()
 # should union two disjunct configuratuions
-def test_merge_configurations():
+def test_merge_configurations(conf: Configuration):
     skip()
