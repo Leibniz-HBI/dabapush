86bbeb1d5b7879256bac65e56c4a65cea80e4762
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-08-27 14:26:47 +0200

    [fix] use `skip()` instead of  `pass`

--- tests/Configuration/test_Configuration.py
+++ tests/Configuration/test_Configuration.py
@@ -1,37 +1,38 @@
+from pytest import fixture, skip
 from dabapush.Configuration.Configuration import Configuration
 
 def test_Configuration():
-    pass
+    skip()
 # should serialize
 def test_serialize():
-    pass
+    skip()
 # should deserialize
 def test_deserialize():
-    pass
+    skip()
 # should get a reader plugin and return it
 def test_get_reader():
-    pass
+    skip()
 # should register a reader plugin and assign it a name
 def test_register_reader():
-    pass
+    skip()
 # should remove reader plugin by name
 def test_remove_reader():
-    pass
+    skip()
 # should list registered reader plugins
 def test_list_reader():
-    pass
+    skip()
 # should register writer plugins and assign a name
 def test_register_writer():
-    pass
+    skip()
 # should remove writer plugins by name
 def test_remove_writer():
-    pass
+    skip()
 # should list registered writer plugins
 def test_list_writers():
-    pass
+    skip()
 # should get a writer plugin and return it
 def test_get_writer():
-    pass
+    skip()
 # should union two disjunct configuratuions
 def test_merge_configurations():
-    pass
+    skip()
