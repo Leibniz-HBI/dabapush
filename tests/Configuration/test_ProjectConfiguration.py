2b2a7b2fefea7437a251752c273113b56f759544
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-08-27 14:34:40 +0200

    [add] more fixtures

--- tests/Configuration/test_ProjectConfiguration.py
+++ tests/Configuration/test_ProjectConfiguration.py
@@ -1,43 +1,55 @@
+from collections import ChainMap
+from pytest import fixture, skip
 from dabapush.Configuration.ProjectConfiguration import ProjectConfiguration
 from dabapush.Configuration.Configuration import Configuration
 import pytest
 
-
 @pytest.fixture
-def conf():
+def conf() -> ProjectConfiguration:
+    configuration = Configuration(
+        ChainMap(        {
+            "Twacapic": 'dabapush.Reader.TwacapicReader'
+        })
+    )
     project = ProjectConfiguration()
-    project.initialize(Configuration())
+    project.initialize(configuration)
     return project
 
+@fixture
+def conf_loaded_single(conf: ProjectConfiguration) -> ProjectConfiguration:
+    conf.add_reader('Twacapic', 'name')
+
+    return conf
+
 def test_ProjectConfiguration(conf):
-    pass
+    skip()
 # should serialize
 def test_serialize(conf):
-    pass
+    skip()
 # should deserialize
 def test_deserialize(conf):
-    pass
+    skip()
 # should add reader plugin and assign it an ID and name
-def test_add_reader(conf):
+def test_add_reader(conf: ProjectConfiguration):
     id = conf.add_reader('Twacapic', 'name')
 
-    assert 'Twacapic' in conf.readers
+    assert 'name' in conf.readers
 
-# should remove reader plugin by ID or by name
-def test_remove_reader(conf):
-    id = conf.add_reader('Twacapic', 'name')
-    conf.remove_reader(id)
+# should remove reader plugin by name
+def test_remove_reader(conf_loaded_single: ProjectConfiguration):
+    conf_loaded_single.remove_reader('name')
 
-    assert not 'Twacapic' in conf.readers
+    assert 'name' not in conf_loaded_single.readers
 # should list configured reader plugins
-def test_list_reader(conf):
-    pass
+def test_list_reader(conf_loaded_single: ProjectConfiguration):
+    entries = conf_loaded_single.list_readers()
+    assert entries[0].startswith('name')
 # should add writer plugins and assign it an ID and name
 def test_add_writer(conf):
-    pass
+    skip()
 # should remove writer plugins by ID or name
 def test_remove_writer(conf):
-    pass
+    skip()
 # should list configured writer plugins
 def test_list_writers(conf):
-    pass
+    skip()
