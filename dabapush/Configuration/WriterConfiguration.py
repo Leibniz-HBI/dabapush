35794646b21f1cbffb4884f6a611423dde78572d
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-08-27 17:22:17 +0200

    [fix] buggy bug with namey passy

--- dabapush/Configuration/WriterConfiguration.py
+++ dabapush/Configuration/WriterConfiguration.py
@@ -3,4 +3,4 @@ from .PlugInConfiguration import PlugInConfiguration
 class WriterConfiguration(PlugInConfiguration):
     """ """
     def __init__(self, name, id=None) -> None:
-        super().__init__(id=id)
+        super().__init__(name, id=id)
