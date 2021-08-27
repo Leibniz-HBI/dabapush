0a329bf4ede6b17e2fab4addc047bd2032c94a9c
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-08-27 17:21:40 +0200

    [add] add'em tests

--- tests/Configuration/test_ProjectConfiguration.py
+++ tests/Configuration/test_ProjectConfiguration.py
@@ -1,4 +1,3 @@
-from collections import ChainMap
 from pytest import fixture, skip
 from dabapush.Configuration.ProjectConfiguration import ProjectConfiguration
 from dabapush.Configuration.Configuration import Configuration
@@ -7,17 +6,25 @@ import pytest
 @pytest.fixture
 def conf() -> ProjectConfiguration:
     configuration = Configuration(
-        ChainMap(        {
+        {
             "Twacapic": 'dabapush.Reader.TwacapicReader'
-        })
+        }, {
+            'CSV': 'dabpush.Writer.CSVWriter'
+        }
     )
     project = ProjectConfiguration()
     project.initialize(configuration)
     return project
 
 @fixture
-def conf_loaded_single(conf: ProjectConfiguration) -> ProjectConfiguration:
+def conf_loaded_single_reader(conf: ProjectConfiguration) -> ProjectConfiguration:
     conf.add_reader('Twacapic', 'name')
+
+    return conf
+
+@fixture
+def conf_loaded_single_writer(conf: ProjectConfiguration) -> ProjectConfiguration:
+    conf.add_writer('CSV', 'name')
 
     return conf
 
@@ -34,23 +41,31 @@ def test_add_reader(conf: ProjectConfiguration):
     assert 'name' in conf.readers
 
 # should remove reader plugin by name
-def test_remove_reader(conf_loaded_single: ProjectConfiguration):
-    conf_loaded_single.remove_reader('name')
+def test_remove_reader(conf_loaded_single_reader: ProjectConfiguration):
+    conf_loaded_single_reader.remove_reader('name')
 
-    assert 'name' not in conf_loaded_single.readers
+    assert 'name' not in conf_loaded_single_reader.readers
 # should list configured reader plugins
-def test_list_reader(conf_loaded_single: ProjectConfiguration):
-    entries = conf_loaded_single.list_readers()
+def test_list_reader(conf_loaded_single_reader: ProjectConfiguration):
+    entries = conf_loaded_single_reader.list_readers()
     assert entries[0].name == 'name'
 # should add writer plugins and assign it an ID and name
-def test_add_writer(conf):
-    skip()
+def test_add_writer(conf: ProjectConfiguration):
+    conf.add_writer('CSV', "name")
+
+    assert 'name' in conf.writers
+
 # should remove writer plugins by ID or name
-def test_remove_writer(conf):
-    skip()
+def test_remove_writer(conf_loaded_single_writer: ProjectConfiguration):
+    conf_loaded_single_writer.remove_writer('name')
+
+    assert 'name' not in conf_loaded_single_writer.writers
+
 # should list configured writer plugins
-def test_list_writers(conf):
-    skip()
+def test_list_writers(conf_loaded_single_writer: ProjectConfiguration):
+    ret = conf_loaded_single_writer.list_writers()
+
+    assert 'name' == ret[0].name
 
 def test_initialize():
     # we expect ProjectConfig to raise an exception
