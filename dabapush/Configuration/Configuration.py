8b17ff606fae3a9d02d0e0db6d283b1ff5fd32da
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-08-27 13:11:18 +0200

    [add] implementation with chainmaps

--- dabapush/Configuration/Configuration.py
+++ dabapush/Configuration/Configuration.py
@@ -1,16 +1,45 @@
 from pathlib import Path
-from typing import Mapping
+from typing import ChainMap, List
+from dabapush.Reader.Reader import Reader
+from dabapush.Writer.Writer import Writer
 import yaml
-
+import collections
 class Configuration(yaml.YAMLObject):
-    def __init__(self) -> None:
+    def __init__(self, readers: ChainMap[str, Reader] = collections.ChainMap(), writers: ChainMap[str, Writer] = collections.ChainMap()) -> None:
         super().__init__()
+
+        self.readers = readers
+        self.writers = writers
 
     def __repr__(self) -> str:
         return super().__repr__()
 
-    
+    def get_reader(self, type: str) -> Reader:
+        pass
 
+    def get_writer(self, type: str) -> Writer:
+        pass
+
+    def register_reader(self, name: str, constructor) -> None:
+        pass
+
+    def register_writer(self, name: str, constructor) -> None:
+        pass
+
+    def remove_reader(self, name: str) -> bool:
+        pass
+
+    def remove_writer(self, name: str) -> bool:
+        pass
+
+    def list_writers(self) -> List[str]:
+        return ['a','b','c']
+
+    def list_writers(self) -> List[str]:
+        pass
+
+    def merge_with(conf: 'Configuration') -> 'Configuration':
+        pass
 
 
 # class DabapushConfiguration(object):
