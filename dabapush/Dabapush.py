0d6561aaf7bbcb2f89ad8be6ac29a1c09af61fa6
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-08-31 18:16:46 +0200

    [add] default working dir

--- dabapush/Dabapush.py
+++ dabapush/Dabapush.py
@@ -1,9 +1,10 @@
 from loguru import logger as log
+from pathlib import Path
 
 class Dabapush(object):
     """ This is the main class for this application.
 
-    It is a Singleton pattern class and follwos the interface pattern as well.
+    It is a Singleton pattern class and follows the interface pattern as well.
     
     
     """
@@ -12,9 +13,13 @@ class Dabapush(object):
 
     def __new__(
         cls,
-        working_dir: Path
+        working_dir: Path = Path()
     ):
         if (cls.__instance__ is None):
             cls.__instance__ = super(Dabapush, cls).__new__(cls)
             # init code here: ...
+            cls.__instance__.working_dir = working_dir
         return cls.__instance__
+
+    def update_reader_targets(self, name: str) -> None:
+        pass
