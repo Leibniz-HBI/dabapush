9772a2674d918dd4fa71cba64471d6b1a84a5ab9
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-08-31 18:17:24 +0200

    [add] a few tests

--- tests/test_Dabapush.py
+++ tests/test_Dabapush.py
@@ -1,12 +1,24 @@
 from pytest import fixture, skip
+from pathlib import Path
+from dabapush.Dabapush import Dabapush
+
+@fixture
+def dabapush():
+    return Dabapush()
 
 # should be a Singleton (DONE)
-# should be a Interface
+def test_singleton(dabapush):
+    assert dabapush == Dabapush()
+# should be a Interface?
 # should 
 # should accept ProjectConfiguration and Configuration 
 # should accept a working directory
+def test_working_directory(dabapush):
+    assert dabapush.working_dir == Path()
 # should accept a installation directory (or a directory for a global conf)
 # should dispatch changes in the configuration
 # should acquire targets
 # should dispatch jobs
-# should
+# should update targets per jop
+# should persistently keep state for targets and jobs
+
