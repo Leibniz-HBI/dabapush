b6c59d77547ca05e614a9439b31f6e7f3d76c2de
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-10-15 11:43:10 +0200

    [fix] the indention because python

--- dabapush/create_subcommand.py
+++ dabapush/create_subcommand.py
@@ -30,22 +30,22 @@ def create(ctx, interactive):
             click.prompt('author name (split several authors with ";")', type=str)
         )
 
-    man_config = click.confirm("Should we configure readers and writers?")
-    while (man_config == True):
-        thing_to_configure = click.prompt('Reader/Writer?', default='Writer')
-        if (thing_to_configure != 'Reader' and thing_to_configure != 'Writer'):
-            log.debug(f'Try again')
-        else:
-            if (thing_to_configure == 'Reader'):
-                log.debug(f'Configuring a Reader')
-                reader_name = click.prompt('Which Reader should we configure')
-                if (reader_name in globconf['plugins']['reader']):
-                    if (not 'reader' in conf):
-                        conf["reader"] = {}
-                    conf["reader"]["someid"] = reader_name
-                    log.debug(f'Success! Found the reader you\'re looking for!')
-            if (thing_to_configure == 'Writer'):
-                log.debug(f'Configuring a Reader')
-            man_config = click.confirm('do another')
+        man_config = click.confirm("Should we configure readers and writers?")
+        while (man_config == True):
+            thing_to_configure = click.prompt('Reader/Writer?', default='Writer')
+            if (thing_to_configure != 'Reader' and thing_to_configure != 'Writer'):
+                log.debug(f'Try again')
+            else:
+                if (thing_to_configure == 'Reader'):
+                    log.debug(f'Configuring a Reader')
+                    reader_name = click.prompt('Which Reader should we configure')
+                    if (reader_name in globconf['plugins']['reader']):
+                        if (not 'reader' in conf):
+                            conf["reader"] = {}
+                        conf["reader"]["someid"] = reader_name
+                        log.debug(f'Success! Found the reader you\'re looking for!')
+                if (thing_to_configure == 'Writer'):
+                    log.debug(f'Configuring a Reader')
+                man_config = click.confirm('do another')
     with ctx.obj["locconf_path"].open('w') as file:
         yaml.dump(conf, file)
