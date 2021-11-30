9bf205f18ac8c1cfc19184ade2d1518bbfea40a0
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-11-30 15:54:39 +0100

    [update] DabaPush implementation

--- dabapush/Dabapush.py
+++ dabapush/Dabapush.py
@@ -23,63 +23,63 @@ class Dabapush(object):
         pass
   
     # PROJECT specific methods
-    def pr_init():
+    def pr_init(self):
         """
         Initliaze a new project in the current directory
         """
         pass
-    def pr_write():
+    def pr_write(self):
         """
         Write the current configuration to the project configuration file in the current directory
         """
         pass
-    def pr_read():
+    def pr_read(self):
         """
         Read the project configuration file in the current directory
         """
         pass
        
     # READER specific methods
-    def rd_add():
+    def rd_add(self):
         """
         add a reader to the current project
         """
         pass
-    def rd_rm():
+    def rd_rm(self):
         """
         remove a reader from the current configuration
         """
         pass
-    def rd_update():
+    def rd_update(self):
         """
         update a reader's configuration
         """
         pass
 
     # WRITER specific methods
-    def wr_add():
+    def wr_add(self):
         """
         add a reader to the current project
         """
         pass
-    def wr_rm():
+    def wr_rm(self):
         """
         remove a reader from the current configuration
         """
         pass
-    def wr_update():
+    def wr_update(self):
         """
         update a reader's configuration
         """
         pass
 
     # JOB specific methods
-    def jb_run():
+    def jb_run(self):
         """
         run the job configured in the current directory
         """
         pass
-    def jb_update():
+    def jb_update(self):
         """
         update the current job's targets
         """
