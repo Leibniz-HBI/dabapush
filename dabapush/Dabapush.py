02753c34ffc776a969f958daae809d0ed861e129
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-11-30 09:14:41 +0100

    [add] DabaPush methods

--- dabapush/Dabapush.py
+++ dabapush/Dabapush.py
@@ -5,8 +5,6 @@ class Dabapush(object):
     """ This is the main class for this application.
 
     It is a Singleton pattern class and follows the interface pattern as well.
-    
-    
     """
 
     __instance__ = None
@@ -22,4 +20,67 @@ class Dabapush(object):
         return cls.__instance__
 
     def update_reader_targets(self, name: str) -> None:
+        pass
+  
+    # PROJECT specific methods
+    def pr_init():
+        """
+        Initliaze a new project in the current directory
+        """
+        pass
+    def pr_write():
+        """
+        Write the current configuration to the project configuration file in the current directory
+        """
+        pass
+    def pr_read():
+        """
+        Read the project configuration file in the current directory
+        """
+        pass
+       
+    # READER specific methods
+    def rd_add():
+        """
+        add a reader to the current project
+        """
+        pass
+    def rd_rm():
+        """
+        remove a reader from the current configuration
+        """
+        pass
+    def rd_update():
+        """
+        update a reader's configuration
+        """
+        pass
+
+    # WRITER specific methods
+    def wr_add():
+        """
+        add a reader to the current project
+        """
+        pass
+    def wr_rm():
+        """
+        remove a reader from the current configuration
+        """
+        pass
+    def wr_update():
+        """
+        update a reader's configuration
+        """
+        pass
+
+    # JOB specific methods
+    def jb_run():
+        """
+        run the job configured in the current directory
+        """
+        pass
+    def jb_update():
+        """
+        update the current job's targets
+        """
         pass
