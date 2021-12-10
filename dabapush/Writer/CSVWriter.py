228883fd09fe118e4c74566acb7eb1edcf1fe5c4
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-12-10 11:09:36 +0100

    [add] CSVWriterCofiguration

--- dabapush/Writer/CSVWriter.py
+++ dabapush/Writer/CSVWriter.py
@@ -1,30 +1,48 @@
 from loguru import logger as log
 from pathlib import Path
 from .Writer import Writer
+from ..Configuration.WriterConfiguration import WriterConfiguration
+
+class CSVWriterConfiguration(WriterConfiguration):
+    type = "CSV" # static of CSVWriterConfiguration
+
+    def __init__(self, name, id=None, chunk_size: int = 2000, path: Path = Path()) -> None:
+        super().__init__(name, id=id)
+        
+        self.chunk_size = chunk_size
+        self.path = path
+        self.name_template # template for string interpolation for the filename
+
+    @property
+    def file_path(self) -> Path:
+        # evalutate self.name_template
+        file_name = self.name_template
+        # append to self.path and return
+        return self.path / file_name
 
 class CSVWriter(Writer):
     """ """
     
-    def __init__(self):
+    def __init__(self, config: CSVWriterConfiguration):
         super().__init__()
+        self.config = config
 
-    def persist(self, chunkSize: int):
+    def persist(self):
         """
 
         Args:
-          chunkSize: int: 
 
         Returns:
 
         """
 
-        last_row = self.buffer.head(chunkSize)
+        last_row = self.buffer.head(self.config.chunk_size)
         self.buffer.drop(last_row.index, inplace=True)
 
         log.info(f'Persisted {len(last_row)} records')
         
         # TODO: make path configurable!!!
-        with self.path.open('a') as file:
+        with self.config.file_path.open('a') as file:
             last_row.replace(r'\n|\r', r'\\n', regex=True, inplace=True)
             last_row[self.schema].to_csv(file, index=False, header=False)
         return len(last_row)
