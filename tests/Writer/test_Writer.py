166c8df261ef0f6cd501cd0ab2876f03deea1e33
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-12-10 13:51:37 +0100

    [add] unit tests for Writer

---
+++ tests/Writer/test_Writer.py
@@ -0,0 +1,14 @@
+from pytest import fixture, skip, mark
+from dabapush.Writer.Writer import Writer
+from dabapush.Configuration.WriterConfiguration import WriterConfiguration
+
+@fixture
+def writer() -> Writer:
+    config = WriterConfiguration(name = 'test')
+    return Writer(config)
+
+def test_name(writer: Writer):
+    assert writer.name == 'test'
+
+def test_id(writer: Writer):
+    assert writer.id == writer.config.id
