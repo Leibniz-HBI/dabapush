c559a19d1a409a2a6a66fb12c8b9167fb4740b5c
Philipp Kessling <p.kessling@leibniz-hbi.de>
2021-12-23 12:37:57 +0100

    [add] run implementation

--- dabapush/run_subcommand.py
+++ dabapush/run_subcommand.py
@@ -1,20 +1,24 @@
 import click
 from loguru import logger as log
+from .Dabapush import Dabapush
 
-@click.command(help="Run the specified target, if specified target is 'all' all targets are run.")
-@click.argument('target')
+@click.command(help="Run dabapush job in the current working directory.")
+@click.argument('targets', nargs=-1)
 @click.pass_context
-def run(ctx, target):
+def run(ctx, targets: list[str]) -> None:
     """
 
     Args:
       ctx: 
-      target: 
 
     Returns:
 
     """
-    log.debug(f'Runrunrun {target} in {ctx.obj["wd"]}')
-    log.debug(f'Using this global configuration {ctx.obj["globconf"]}')
-    log.debug(f'Using this local configuration {ctx.obj["locconf"]}')
-    pass
+    db: Dabapush = ctx.obj
+    log.debug(f'Running DabaPush job in {db.working_dir}')
+    # log.debug(f'Using this global configuration {db.global_config}')
+    # log.debug(f'Using this local configuration {db.config}')
+    
+    db.jb_run(targets)
+
+    
