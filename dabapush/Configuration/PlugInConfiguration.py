208568b72115da252e6245ff66fa8d133ec97955
Philipp Kessling <p.kessling@leibniz-hbi.de>
2022-02-15 20:27:57 +0100

    [add] abstract method to PlugInConf

--- dabapush/Configuration/PlugInConfiguration.py
+++ dabapush/Configuration/PlugInConfiguration.py
@@ -1,12 +1,11 @@
 import yaml
 from uuid import UUID, uuid4
+from abc import abstractclassmethod
 
 class PlugInConfiguration(yaml.YAMLObject):
     """ """
     
     yaml_tag = '!dabapush:PluginConfiguration'
-
-    type = "NONE"
 
     def __init__(self, name: str, id: str or None) -> None:
         super().__init__()
@@ -16,3 +15,7 @@ class PlugInConfiguration(yaml.YAMLObject):
 
     def __repr__(self) -> str:
         return super().__repr__()
+
+    @abstractclassmethod
+    def get_instance(self) -> 'Reader' or 'Writer':
+        pass
