c2409a8830c9cab5c4ccfb9524d8ef12fae79f8d
Philipp Kessling <p.kessling@leibniz-hbi.de>
2022-02-15 20:26:34 +0100

    [update] writing logic

--- dabapush/Writer/NDJSONWriter.py
+++ dabapush/Writer/NDJSONWriter.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from string import Template
 from loguru import logger as log
 
 from ..Configuration.FileWriterConfiguration import FileWriterConfiguration
@@ -23,10 +24,12 @@ class NDJSONWriter(Writer):
         last_rows = self.buffer
         self.buffer = []
         
+        _file: Path = Path(self.config.write_path) / self.config.make_file_name()
         
-        with self.path.open('a') as file:
+        with _file.open('a', encoding='utf8') as file:
             for row in last_rows:
-                ujson.dump(row, file)
+                ujson.dump(row, file, ensure_ascii=False)
+                file.write('\n')
         log.info(f'Persisted {len(last_rows)} records')
         
         return len(last_rows)
@@ -35,8 +38,10 @@ class NDJSONWriterConfiguration(FileWriterConfiguration):
 
     yaml_tag = '!dabapush:NDJSONWriterConfiguration'
 
-    def __init__(self, name, id=None, chunk_size: int = 2000, path: Path = ..., name_template: str = "__.") -> None:
+    def __init__(self, name, id=None, chunk_size: int = 2000, path: str = '.', name_template: str = "${date}_${time}_${name}.${type}") -> None:
         super().__init__(name, id, chunk_size, path, name_template)
+        self.name_template = Template(name_template)
+        self.type = 'ndjson'
 
     def get_instance(self):
         return NDJSONWriter(self)
