922b2673eca84c688fbda08d02c1506b4bbf7372
Aditya Kumar <ads1996.k@gmail.com>
2022-03-01 15:45:30 +0100

    Facebook Writer

--- dabapush/Writer/FacebookDBWriter.py
+++ dabapush/Writer/FacebookDBWriter.py
@@ -1,5 +1,5 @@
 from .Writer import Writer
-from ...smo_database import DB_Manager
+from ...smo_database import DB_Manager, Facebook_Data
 
 class FacebookDBWriter(Writer):
     """ """
@@ -7,7 +7,7 @@ class FacebookDBWriter(Writer):
     def __init__(self, config):
         super().__init__()
         
-        self.initialze_db = DB_Manager(config)
+        self.initialize_db = DB_Manager(config)
         self.engine = self.initialize_db.create_connection()
         self.facebook_initializer = Facebook_Data(self.engine)
         self.facebook_initializer.create_local_session()
