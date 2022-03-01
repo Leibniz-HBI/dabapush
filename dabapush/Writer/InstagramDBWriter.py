24576ca2be005ab9a78b98a3735f6f6f54c26e49
Aditya Kumar <ads1996.k@gmail.com>
2022-03-01 15:45:43 +0100

    Instagram Writer

--- dabapush/Writer/InstagramDBWriter.py
+++ dabapush/Writer/InstagramDBWriter.py
@@ -1,5 +1,5 @@
 from .Writer import Writer
-from ...smo-database
+from ...smo_database import DB_Manager, Instagram_Data
 
 class InstagramDBWriter(Writer):
     """ """
@@ -7,10 +7,10 @@ class InstagramDBWriter(Writer):
     def __init__(self, config):
         super().__init__()
         
-        self.initialze_db = DBManager(config)
+        self.initialize_db = DB_Manager(config)
         self.engine = self.initialize_db.create_connection()
         self.instagram_initializer = Instagram_Data(self.engine)
-        self.session = self.instagram_initializer.create_local_session()
+        self.instagram_initializer.create_local_session()
 
     def persist(self):
         """
@@ -24,4 +24,8 @@ class InstagramDBWriter(Writer):
         data = self.buffer
         self.buffer = []
 
-        self.instagram_initializer.fb_insert(data)
+        self.instagram_initializer.insta_insert(data)
+    
+    def close_local_session(self):
+        
+        self.instagram_initializer.close_local_session()
