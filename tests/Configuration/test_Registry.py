3615622339c905e0bde44543a9ace5ad74e3b2f8
Philipp Kessling <p.kessling@leibniz-hbi.de>
2022-11-09 15:02:42 +0100

    [update] make those tests green

--- tests/Configuration/test_Registry.py
+++ tests/Configuration/test_Registry.py
@@ -1,19 +1,25 @@
+"""Test suite for dabapush.Configuration.Registry."""
+# pylint: disable=W0621
+import pytest
 from pytest import fixture, skip
+
 from dabapush.Configuration.ProjectConfiguration import ProjectConfiguration
 from dabapush.Configuration.Registry import Registry
-import pytest
-import yaml
 
 
 @pytest.fixture
 def conf() -> ProjectConfiguration:
-    Registry()
+    """Provide a ProjectConfiguration."""
+    reg = Registry()
+
+    print(reg.list_all_readers())
 
     return ProjectConfiguration()
 
 
 @fixture
 def conf_loaded_single_reader(conf: ProjectConfiguration) -> ProjectConfiguration:
+    """Provide a ProjectConfiguration with exactly one Reader."""
     conf.add_reader("Twacapic", "name")
 
     return conf
@@ -21,69 +27,69 @@ def conf_loaded_single_reader(conf: ProjectConfiguration) -> ProjectConfiguratio
 
 @fixture
 def conf_loaded_single_writer(conf: ProjectConfiguration) -> ProjectConfiguration:
+    """Provide a ProjectConfiguration with exactly one Writer."""
     conf.add_writer("CSV", "name")
     return conf
 
 
-# should serialize
-def test_serialize(conf: ProjectConfiguration):
-    skip()
-    thing = yaml.dump(conf)
-    print(thing)
-
-
-# should deserialize
-def test_deserialize(conf):
+def test_serialize():
+    """Should serialize."""
     skip()
 
 
-# should add reader plugin and assign it an ID and name
+def test_deserialize():
+    """Should deserialize."""
+    skip()
+
+
 def test_add_reader(conf: ProjectConfiguration):
-    id = conf.add_reader("Twacapic", "name")
+    """Should add reader plugin and assign it an ID and name."""
+    conf.add_reader("Twacapic", "name")
     assert "name" in conf.readers
 
 
-# should remove reader plugin by name
 def test_remove_reader(conf_loaded_single_reader: ProjectConfiguration):
+    """Should remove reader plugin by name."""
     conf_loaded_single_reader.remove_reader("name")
 
     assert "name" not in conf_loaded_single_reader.readers
 
 
-# should list configured reader plugins
 def test_list_reader(conf_loaded_single_reader: ProjectConfiguration):
+    """Should list configured reader plugins."""
     entries = conf_loaded_single_reader.list_readers()
-    assert entries[0].name == "name"
+    assert "name" in [_.name for _ in entries]
 
 
-# should add writer plugins and assign it an ID and name
 def test_add_writer(conf: ProjectConfiguration):
+    """Should add writer plugins and assign it an ID and name."""
     conf.add_writer("CSV", "name")
     assert "name" in conf.writers
 
 
-# should remove writer plugins by ID or name
 def test_remove_writer(conf_loaded_single_writer: ProjectConfiguration):
+    """Should remove writer plugins by ID or name."""
     conf_loaded_single_writer.remove_writer("name")
 
     assert "name" not in conf_loaded_single_writer.writers
 
 
-# should list configured writer plugins
 def test_list_writers(conf_loaded_single_writer: ProjectConfiguration):
+    """Should list configured writer plugins."""
     ret = conf_loaded_single_writer.list_writers()
 
     assert "name" == ret[0].name
 
 
 def test_initialize():
+    """Should raise an Exception if accessed prematurely."""
     # we expect ProjectConfig to raise an exception
-    # if accessed bfore intializiation
+    # if accessed before intializiation
     raised = False
     try:
-        p = ProjectConfiguration()
-        p.add_reader("Thing", "thing")
-    except:
+        project = ProjectConfiguration()
+        project.add_reader("Thing", "thing")
+    except BaseException:
         raised = True
 
-    assert raised == True
+    assert raised is True
