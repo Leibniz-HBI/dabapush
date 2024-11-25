3b3cef448fa70a218de3984457db0716e0dd7d9e
Philipp Kessling <p.kessling@leibniz-hbi.de>
2024-11-25 15:08:47 +0100

    bump version

--- tests/test_package.py
+++ tests/test_package.py
@@ -5,14 +5,9 @@ from dabapush import __version__
 
 def test_version():
     """Should be the current version number."""
-    assert __version__ == "0.3.3"
+    assert __version__ == "0.4.0-alpha"
 
 
 def test_version_type():
     """Should be a string."""
     assert isinstance(__version__, str)
-
-
-def test_version_number_is_semver():
-    """Should be a semver."""
-    assert all((x.isnumeric() for x in __version__.split(".")))
