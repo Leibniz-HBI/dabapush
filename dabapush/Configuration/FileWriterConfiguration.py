c167f6c208b80ba520b28441ea6adb7dff03510a
Philipp Kessling <p.kessling@leibniz-hbi.de>
2024-11-26 09:53:33 +0100

    feat: add unit tests for CSVWriter .

--- dabapush/Configuration/FileWriterConfiguration.py
+++ dabapush/Configuration/FileWriterConfiguration.py
@@ -1,9 +1,15 @@
 """FileWriterConfiguration provides a base class for file-based Writers."""
+
+import abc
 from datetime import datetime
 from string import Template
 from typing import Dict, Optional
 
+from loguru import logger as log
+
 from .WriterConfiguration import WriterConfiguration
+
+# pylint: disable=W0221,W0622,R0917,R0913
 
 
 class FileWriterConfiguration(WriterConfiguration):
@@ -33,15 +39,18 @@ class FileWriterConfiguration(WriterConfiguration):
           Interpolated file name as str.
         """
         now = datetime.now()
-        return Template(self.name_template).substitute(
-            **{
-                "date": datetime.strftime(now, "%Y-%m-%d"),
-                "time": datetime.strftime(now, "%H%M"),
-                "name": self.name,
-                "id": self.id,
-                **(additional_keys or {}),
-            }
-        )
+        available_data = {
+            "date": datetime.strftime(now, "%Y-%m-%d"),
+            "time": datetime.strftime(now, "%H%M"),
+            "chunk_size": self.chunk_size,
+            "name": self.name,
+            "id": self.id,
+            **(additional_keys or {}),
+        }
+
+        log.info(f"Available data: {available_data}")
+
+        return Template(self.name_template).substitute(**available_data)
 
     def set_name_template(self, template: str):
         """Sets the template string.
@@ -51,3 +60,7 @@ class FileWriterConfiguration(WriterConfiguration):
             Template string to use.
         """
         self.name_template = template
+
+    @abc.abstractmethod
+    def get_instance(self) -> object or None:
+        """Get configured instance of Writer"""
