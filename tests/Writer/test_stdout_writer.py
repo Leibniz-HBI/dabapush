bd2587d1948f4eb7f734ef9525adf608954a2131
Philipp Kessling <p.kessling@leibniz-hbi.de>
2025-02-11 21:30:34 +0100

    Update test_std_writer.py
    Run test in an isolated directory to avoid database collision between runs.

--- tests/Writer/test_stdout_writer.py
+++ tests/Writer/test_stdout_writer.py
@@ -4,7 +4,7 @@ from dabapush import STDOUTWriterConfiguration
 from dabapush.Record import Record
 
 
-def test_stdout_writer(capsys):
+def test_stdout_writer(capsys, isolated_test_dir):  # pylint: disable=unused-argument
     """Should write to stdout."""
     writer = STDOUTWriterConfiguration("stdout1").get_instance()
     writer.buffer = [Record(uuid="test.01", source=None, payload="test")]
